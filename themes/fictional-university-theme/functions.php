ec523d60e4ab12196e2be5d07ff77ef3cc667cee
Krupa <joshikrupa2711@gmail.com>
2024-03-11 19:00:55 +0530

    convert static field to dynamic fields and refactored code

--- themes/fictional-university-theme/functions.php
+++ themes/fictional-university-theme/functions.php
@@ -1,5 +1,39 @@
 <?php
 
+function pageBanner($args = NULL)
+{
+    if (is_null($args)) {
+        $args = array();
+    }
+
+    if (!array_key_exists("title", $args)) {
+        $args['title'] = get_the_title();
+    }
+
+    if (!array_key_exists('subtitle', $args)) {
+        $args['subtitle'] = get_field('page_banner_subtitle');
+    }
+
+    if (!isset($_FILES['photo'])) {
+        if (get_field('page_banner_background_image')) {
+            $args['photo'] = get_field('page_banner_background_image')['sizes']['pageBanner'];
+        } else {
+            $args['photo'] = get_theme_file_uri('/images/ocean.jpg');
+        }
+    }
+
+?>
+    <div class="page-banner">
+        <div class="page-banner__bg-image" style="background-image: url(<?php echo $args['photo']; ?>);"></div>
+        <div class="page-banner__content container container--narrow">
+            <h1 class="page-banner__title"><?php echo $args['title'] ?></h1>
+            <div class="page-banner__intro">
+                <p><?php echo $args['subtitle']; ?></p>
+            </div>
+        </div>
+    </div>
+<?php }
+
 function university_files()
 {
     wp_enqueue_script('main-university-js', get_theme_file_uri('/build/index.js'), array('jquery'), '1.0', true);
@@ -14,16 +48,20 @@ add_action('wp_enqueue_scripts', 'university_files');
 function university_features()
 {
     add_theme_support('title-tag');
+    add_theme_support('post-thumbnails');
+    add_image_size('professorLandscape', 400, 260, true);
+    add_image_size('professorPortrait', 480, 650, true);
+    add_image_size('pageBanner', 1500, 350, true);
 }
 
 add_action('after_setup_theme', 'university_features');
 
 function university_adjust_queries($query)
 {
-    if (!is_admin() and is_post_type_archive('program') and is_main_query()) {
+    if (!is_admin() and is_post_type_archive('program') and $query->is_main_query()) {
         $query->set('orderby', 'title');
         $query->set('order', 'ASC');
-        $query->set('Posts_per_page', '-1');
+        $query->set('posts_per_page', -1);
     }
 
     if (!is_admin() and is_post_type_archive('event') and $query->is_main_query()) {
@@ -32,10 +70,12 @@ function university_adjust_queries($query)
         $query->set('orderby', 'meta_value_num');
         $query->set('order', 'ASC');
         $query->set('meta_query', array(
-            'key' => 'event_date',
-            'compare' => '>=',
-            'value' => $today,
-            'type' => 'numeric'
+            array(
+                'key' => 'event_date',
+                'compare' => '>=',
+                'value' => $today,
+                'type' => 'numeric'
+            )
         ));
     }
 }
